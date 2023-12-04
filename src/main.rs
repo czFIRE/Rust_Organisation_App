54a2ecda3281a7f4770a293298aa02f7dac176de
Michal Šoltis <msoltis@redhat.com>
2023-12-05 00:21:27 +0100

    Check redis and postgres connection
    Signed-off-by: Michal Šoltis <msoltis@redhat.com>

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,30 @@
-mod handlers;
-mod repositories;
-mod utils;
+use dotenv::dotenv;
+use redis::{Client, ConnectionLike};
+use sqlx::{postgres::PgConnection, Connection};
 
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+    let database_url = dotenv::var("DATABASE_URL").unwrap();
+    let redis_url = dotenv::var("REDIS_URL").unwrap();
+
+    let connection = PgConnection::connect(&database_url).await;
+    match connection {
+        Ok(_) => {
+            println!("Successfully connected to postgres");
+        }
+        Err(_) => {
+            println!("Failed to connect to postgres");
+        }
+    }
+
+    let mut client = Client::open(redis_url).unwrap();
+    match client.check_connection() {
+        true => {
+            println!("Successfully connected to redis");
+        }
+        false => {
+            println!("Failed to connect to redis");
+        }
+    }
 }
