8e7e0c49a79aa43c20451d4ae7a110d5fca4c00c
Michal Šoltis <msoltis@redhat.com>
2023-12-22 10:55:23 +0100

    fix docker build
    Signed-off-by: Michal Šoltis <msoltis@redhat.com>

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,19 @@ use crate::handlers::index::index;
 use crate::repositories::company::CompanyRepository;
 use actix_web::{web, App, HttpServer};
 use dotenv::dotenv;
-use sqlx::migrate::{MigrateDatabase, Migrator};
-use sqlx::Postgres;
-use std::path::Path;
+use std::io::Result;
 use std::sync::Arc;
 
 mod common;
 mod handlers;
 mod repositories;
 
-const HOST: &str = "127.0.0.1:8000";
+const HOST: &str = "0.0.0.0:8000";
 
 #[tokio::main]
-async fn main() -> std::io::Result<()> {
+async fn main() -> Result<()> {
     dotenv().ok();
     let database_url = dotenv::var("DATABASE_URL").expect("DATABASE_URL is not set in .env file");
-    restore_db(&database_url)
-        .await
-        .expect("Failed to restore database");
 
     let pool = sqlx::PgPool::connect(&database_url)
         .await
@@ -30,29 +25,11 @@ async fn main() -> std::io::Result<()> {
     println!("API is running on http://{}/api", HOST);
 
     HttpServer::new(move || {
-        App::new().service(
-            web::scope("/api")
-                .app_data(web::Data::new(CompanyRepository::new(arc_pool.clone())))
-                .service(index),
+        App::new().route("/api", web::get().to(index)).service(
+            web::scope("/api").app_data(web::Data::new(CompanyRepository::new(arc_pool.clone()))),
         )
     })
     .bind(HOST)?
     .run()
     .await
-}
-
-async fn restore_db(url: &str) -> anyhow::Result<()> {
-    Postgres::drop_database(url).await?;
-    println!("Database was successfully dropped");
-
-    Postgres::create_database(url).await?;
-    println!("Database was successfully created");
-
-    let pool = sqlx::PgPool::connect(url).await?;
-
-    let migrator = Migrator::new(Path::new("./migrations")).await?;
-    migrator.run(&pool).await?;
-    println!("Migrations were successfully applied");
-
-    Ok(())
 }
