941ae93fda65105841ec9fb2864f7d3cef240aa1
Matej Vavrek <matvav13@gmail.com>
2023-12-28 14:17:35 +0100

    feat: more skeletons for now

--- tests/api_tests.rs
+++ tests/api_tests.rs
@@ -24,7 +24,7 @@ mod api_tests {
     }
 
     #[actix_web::test]
-    async fn user_create_success() {
+    async fn create_user() {
         let app = test::init_service(App::new().service(organization::handlers::user::create_user)).await;
         // let req = test::TestRequest::default().insert_header(ContentType::plaintext()).to_request();
         let user = json!({
@@ -47,7 +47,7 @@ mod api_tests {
     }
 
     #[actix_web::test]
-    async fn user_create_duplicate_err() {
+    async fn create_user_duplicate() {
         let app = test::init_service(App::new().service(organization::handlers::user::create_user)).await;
         
         let user = json!({
@@ -299,6 +299,11 @@ mod api_tests {
     }
 
     #[actix_web::test]
+    async fn patch_company_empty_data() {
+        todo!()
+    }
+
+    #[actix_web::test]
     async fn delete_company() {
         todo!()
     }
@@ -332,8 +337,94 @@ mod api_tests {
     async fn remove_company_avatar() {
         todo!()
     }
-    //ToDo: Company test
-    //ToDo: Event test
+
+    #[actix_web::test]
+    async fn get_events() {
+        todo!()
+    }
+
+    #[actix_web::test]
+    async fn get_existing_event() {
+        todo!()
+    }
+
+    #[actix_web::test]
+    async fn get_non_existent_event() {
+        todo!()
+    }
+
+    #[actix_web::test]
+    async fn get_event_invalid_uuid_format() {
+        todo!()
+    }
+
+    #[actix_web::test]
+    async fn create_event() {
+        todo!()
+    }
+
+    #[actix_web::test]
+    async fn create_event_duplicate() {
+        todo!()
+    }
+
+    #[actix_web::test]
+    async fn patch_event() {
+        todo!()
+    }
+
+    #[actix_web::test]
+    async fn patch_non_existent_event() {
+        todo!()
+    }
+
+    #[actix_web::test]
+    async fn patch_event_invalid_uuid_format() {
+        todo!()
+    }
+
+    #[actix_web::test]
+    async fn patch_event_empty_data() {
+        todo!()
+    }
+
+    #[actix_web::test]
+    async fn delete_event() {
+        todo!()
+    }
+
+    #[actix_web::test]
+    async fn get_deleted_event() {
+        todo!()
+    }
+
+    #[actix_web::test]
+    async fn delete_non_existent_event() {
+        todo!()
+    }
+
+    #[actix_web::test]
+    async fn delete_event_invalid_uuid_format() {
+        todo!()
+    }
+
+    #[actix_web::test]
+    async fn get_event_avatar() {
+        todo!()
+    }
+
+    #[actix_web::test]
+    async fn upload_event_avatar() {
+        todo!()
+    }
+
+    #[actix_web::test]
+    async fn remove_event_avatar() {
+        todo!()
+    }
+
+
+    
     //ToDo: Task test
     //ToDo: Comment test
     //ToDo: Employment test
