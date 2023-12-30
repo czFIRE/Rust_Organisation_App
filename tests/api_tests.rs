74458a7a9382b75b22d92c2f83199eaee9268a81
Matej Vavrek <matvav13@gmail.com>
2023-12-30 16:23:46 +0100

    feat: get all task comments tests

--- tests/api_tests.rs
+++ tests/api_tests.rs
@@ -1056,6 +1056,7 @@ mod api_tests {
         let body = str::from_utf8(body_bytes.borrow()).unwrap();
         let out = serde_json::from_str::<CommentsTemplate>(body).unwrap();
         assert_eq!(out.comments.len(), 1);
+        assert_eq!(out.comments.first().unwrap().parent_category_id, Uuid::from_str("b71fd7ce-c891-410a-9bb4-70fc5c7748f8").unwrap());
     }
 
     #[actix_web::test]
@@ -1208,19 +1209,43 @@ mod api_tests {
     #[actix_web::test]
     async fn get_all_task_comments() {
         let app = test::init_service(App::new().configure(organization::initialize::configure_app)).await;
-        todo!()
+
+        let req = test::TestRequest::get()
+                            .uri("/task/7ae0c017-fe31-4aac-b767-100d18a8877b/comment")
+                            .to_request();
+        let res = test::call_service(&app, req).await;
+        assert!(res.status().is_success());
+        assert_eq!(res.status(), http::StatusCode::OK);
+
+        let body_bytes = test::read_body(res).await;
+        let body = str::from_utf8(body_bytes.borrow()).unwrap();
+        let out = serde_json::from_str::<CommentsTemplate>(body).unwrap();
+        assert_eq!(out.comments.len(), 1);
+        assert_eq!(out.comments.first().unwrap().parent_category_id, Uuid::from_str("7ae0c017-fe31-4aac-b767-100d18a8877b").unwrap());
     }
 
     #[actix_web::test]
     async fn get_all_task_comments_non_existent_task() {
         let app = test::init_service(App::new().configure(organization::initialize::configure_app)).await;
-        todo!()
+
+        let req = test::TestRequest::get()
+                            .uri("/task/7aecc0d7-fe32-3bdc-b767-100d18a8877b/comment")
+                            .to_request();
+        let res = test::call_service(&app, req).await;
+        assert!(res.status().is_client_error());
+        assert_eq!(res.status(), http::StatusCode::NOT_FOUND);
     }
 
     #[actix_web::test]
     async fn get_all_task_comments_invalid_uuid_format() {
         let app = test::init_service(App::new().configure(organization::initialize::configure_app)).await;
-        todo!()
+
+        let req = test::TestRequest::get()
+                            .uri("/task/INVALIDUUIDFORMATZZZ/comment")
+                            .to_request();
+        let res = test::call_service(&app, req).await;
+        assert!(res.status().is_client_error());
+        assert_eq!(res.status(), http::StatusCode::BAD_REQUEST);
     }
 
     #[actix_web::test]
