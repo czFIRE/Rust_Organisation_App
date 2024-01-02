f621fbbcc3e19699525dd93616a0a04492cfd968
Slavomir Vlcek <svlc@inventati.org>
2024-01-02 20:58:55 +0100

    feat(DB): fill DB with additional rows

--- migrations/data.sql
+++ migrations/data.sql
@@ -17,6 +17,8 @@ DECLARE
   user1_id UUID := '0465041f-fe64-461f-9f71-71e3b97ca85f';
   user2_id UUID := 'ac9bf689-a713-4b66-a3d0-41faaf0f8d0c';
   user3_id UUID := '51a01dbf-dcd5-43a0-809c-94ed8e61d420';
+  user4_id UUID := '30eff81e-a8d9-4ddc-b92e-e4b58e145920';
+  user5_id UUID := 'ced9f31c-8662-4812-9005-b8ae85d3b951';
 
   event0_id UUID := 'b71fd7ce-c891-410a-9bb4-70fc5c7748f8';
   event1_id UUID :='3f152d12-0bbd-429a-a9c5-28967d6370cc';
@@ -26,17 +28,19 @@ DECLARE
   timesheet2_id UUID := 'c51e77aa-bd80-42c7-8b8a-003f018328f6';
   timesheet3_id UUID := '8446b2ba-8223-4388-be5f-9efdfc4ea265';
   timesheet4_id UUID := 'a19a0ac6-3bd2-4ebd-bc8d-ec111ec9f705';
-  timesheet5_id UUID := 'ced9f31c-8662-4812-9005-b8ae85d3b951';
 
   event_staff0_id UUID := '9281b570-4d02-4096-9136-338a613c71cd';
   event_staff1_id UUID := 'a96d1d99-93b5-469b-ac62-654b0cf7ebd3';
   event_staff2_id UUID := 'aa7f3d0e-ab48-473b-ac69-b84cb74f34f7';
+  event_staff3_id UUID := 'ae228dfb-7265-4059-98c4-c4a1e6233cf4';
+  event_staff4_id UUID := '31020fa2-7a24-4d8f-927b-98e26d2929b0';
 
   task0_id UUID := '7ae0c017-fe31-4aac-b767-100d18a8877b';
   task1_id UUID := 'bd9b422d-33c1-42a2-88bf-a56ce6cc55a6';
 
   comment0_id UUID := '0d6cec6a-4fe8-4e44-bf68-e33de0ed121b';
   comment1_id UUID := 'daac23ec-fb36-434a-823b-49716ed2002c';
+  comment2_id UUID := '68c76ba8-86a1-4bc2-8402-2aaa9e4fdb9b';
 
 
 BEGIN
@@ -130,6 +134,24 @@ BEGIN
         'male', 'user', 'available',
         '2023-12-20 08:38:20.288688', '2023-12-20 08:38:20.288688');
 
+    INSERT INTO user_record
+        (id, name, email, birth, avatar_path,
+        gender, role, status,
+        created_at, edited_at)
+        VALUES
+        (user4_id, 'Joanna Dural', 'j.dural@centrum.cz', '1995-08-10', 'joanna.jpg',
+        'female', 'user', 'unavailable',
+        '2023-12-31 09:33', '2024-01-01 12:33');
+
+    INSERT INTO user_record
+        (id, name, email, birth, avatar_path,
+        gender, role, status,
+        created_at, edited_at)
+        VALUES
+        (user5_id, 'Alan Papalochus', 'alan@pap.cz', '1968-08-01', 'alan.jpg',
+        'male', 'user', 'available',
+        '2024-01-01 10:33', '2024-01-01 10:33');
+
     INSERT INTO employment
         (user_id, company_id, manager_id, hourly_wage,
         start_date, end_date, description,
@@ -158,10 +180,32 @@ BEGIN
         type, level,
         created_at, edited_at)
         VALUES
-        (user3_id, company1_id, NULL, 150,
+        (user2_id, company1_id, NULL, 280,
+        '2023-12-31', '2026-01-01', '-',
+        'DPP', 'manager',
+        '2023-12-28 14:30', '2023-12-28 14:30');
+
+    INSERT INTO employment
+        (user_id, company_id, manager_id, hourly_wage,
+        start_date, end_date, description,
+        type, level,
+        created_at, edited_at)
+        VALUES
+        (user3_id, company2_id, NULL, 150,
         '2023-01-01', '2025-01-01', '-',
         'DPP', 'basic',
         '2022-12-29 12:38:20.4', '2023-12-10 14:52:20.1');
+
+    INSERT INTO employment
+        (user_id, company_id, manager_id, hourly_wage,
+        start_date, end_date, description,
+        type, level,
+        created_at, edited_at)
+        VALUES
+        (user5_id, company1_id, NULL, 135,
+        '2024-01-01', '2025-01-01', '-',
+        'DPC', 'basic',
+        '2023-12-30 11:38:20.4', '2023-12-31 14:52');
 
     INSERT INTO event
         (id, name, description,
@@ -174,12 +218,44 @@ BEGIN
         '1969-08-15', '1969-08-18', 'woodstock.png', 
         '2023-05-03 10:38:20.4', '2023-12-01 14:30:20.1');
 
+    INSERT INTO event
+        (id, name, description,
+        website, accepts_staff,
+        start_date, end_date, avatar_path,
+        created_at, edited_at)
+        VALUES
+        (event1_id, 'Darkness 2024', 'Norwegian festival for happy people.',
+        'https://darkness2024.com', true,
+        '2024-01-01', '2024-01-03', 'darkness_2024.png',
+        '2023-06-06 06:06:6.66', '2023-06-06 16:26:6.66');
+
     INSERT INTO associated_company
         (company_id, event_id, type,
         created_at, edited_at)
         VALUES
         (company0_id, event0_id, 'organizer',
         '2023-05-03 10:38:20.4', '2023-12-01 14:30:20.1');
+
+    INSERT INTO associated_company
+        (company_id, event_id, type,
+        created_at, edited_at)
+        VALUES
+        (company0_id, event1_id, 'media',
+        '2023-12-14 08:38:20.4', '2023-12-14 08:38:20.4');
+
+    INSERT INTO associated_company
+        (company_id, event_id, type,
+        created_at, edited_at)
+        VALUES
+        (company1_id, event1_id, 'other',
+        '2023-12-12 07:38:20.4', '2023-12-12 07:38:20.4');
+
+    INSERT INTO associated_company
+        (company_id, event_id, type,
+        created_at, edited_at)
+        VALUES
+        (company2_id, event1_id, 'sponsor',
+        '2023-12-12 12:38:20.4', '2023-12-12 12:38:20.4');
 
     INSERT INTO timesheet
         (id, user_id, company_id, event_id,
@@ -191,6 +267,28 @@ BEGIN
         '1969-08-16', '1969-08-17', 22,
         true, 'not_requested', NULL,
         '1969-08-16 18:26:0.0', '1969-08-17 20:00:0.0');
+
+    INSERT INTO timesheet
+        (id, user_id, company_id, event_id,
+        start_date, end_date, total_hours,
+        is_editable, status, manager_note,
+        created_at, edited_at)
+        VALUES
+        (timesheet1_id, user1_id, company1_id, event0_id,
+        '1969-08-14', '1969-08-17', 24,
+        false, 'accepted', 'Everything seems all righty. Good job.',
+        '2023-05-05 10:39', '2023-05-09 13:39');
+
+    INSERT INTO timesheet
+        (id, user_id, company_id, event_id,
+        start_date, end_date, total_hours,
+        is_editable, status, manager_note,
+        created_at, edited_at)
+        VALUES
+        (timesheet2_id, user3_id, company1_id, event1_id,
+        '2024-01-01', '2024-01-02', 15,
+        false, 'pending', NULL,
+        '2024-01-03 18:26:0.0', '2024-01-03 18:29');
 
     INSERT INTO work_day
         (timesheet_id, date, total_hours, comment, is_editable,
@@ -206,7 +304,40 @@ BEGIN
         (timesheet0_id, '1969-08-16', 10, '', true,
         '1969-08-17 20:00:0.0', '1969-08-17 20:00:0.0');
 
+    INSERT INTO work_day
+        (timesheet_id, date, total_hours, comment, is_editable,
+        created_at, edited_at)
+        VALUES
+        (timesheet1_id, '1969-08-14', 10, '', false,
+        '1969-08-17 19:58:0.0', '1969-08-17 19:59:0.0');
 
+    INSERT INTO work_day
+        (timesheet_id, date, total_hours, comment, is_editable,
+        created_at, edited_at)
+        VALUES
+        (timesheet1_id, '1969-08-15', 4.5, 'I was overworked as a mule!', false,
+        '1969-08-17 20:00', '1969-08-17 20:00');
+
+    INSERT INTO work_day
+        (timesheet_id, date, total_hours, comment, is_editable,
+        created_at, edited_at)
+        VALUES
+        (timesheet1_id, '1969-08-16', 9.5, '', false,
+        '1969-08-17 20:40', '1969-08-17 21:00');
+
+    INSERT INTO work_day
+        (timesheet_id, date, total_hours, comment, is_editable,
+        created_at, edited_at)
+        VALUES
+        (timesheet2_id, '2024-01-01', 8, '', false,
+        '2024-01-01 22:33:5.4', '2024-01-01 22:33:5.4');
+
+    INSERT INTO work_day
+        (timesheet_id, date, total_hours, comment, is_editable,
+        created_at, edited_at)
+        VALUES
+        (timesheet2_id, '2024-01-02', 7, '', false,
+        '2024-01-02 20:10', '2024-01-02 21:12');
 
     INSERT INTO event_staff
         (id, user_id, company_id, event_id,
@@ -222,20 +353,59 @@ BEGIN
         decided_by, role, status,
         created_at, edited_at)
         VALUES
-        (event_staff1_id, user1_id, company0_id, event0_id,
+        (event_staff1_id, user1_id, company1_id, event0_id,
         event_staff0_id, 'staff', 'accepted',
         '2023-05-03 10:40:20.1', '2023-05-04 08:11:20.4');
+
+    INSERT INTO event_staff
+        (id, user_id, company_id, event_id,
+        decided_by, role, status,
+        created_at, edited_at)
+        VALUES
+        (event_staff2_id, user2_id, company1_id, event1_id,
+        event_staff2_id, 'organizer', 'accepted',
+        '2023-06-01 11:40:20.1', '2023-06-01 11:40:20.1');
+
+    INSERT INTO event_staff
+        (id, user_id, company_id, event_id,
+        decided_by, role, status,
+        created_at, edited_at)
+        VALUES
+        (event_staff3_id, user3_id, company2_id, event1_id,
+        event_staff2_id, 'staff', 'pending',
+        '2023-06-02 11:45:20.1', '2023-06-03 19:41:20.1');
+
+    INSERT INTO event_staff
+        (id, user_id, company_id, event_id,
+        decided_by, role, status,
+        created_at, edited_at)
+        VALUES
+        (event_staff4_id, user5_id, company1_id, event1_id,
+        event_staff2_id, 'staff', 'accepted',
+        '2024-01-01 11:45:20.1', '2024-01-01 11:45:20.1');
 
     INSERT INTO task
         (id, event_id, creator_id, title,
         description,
-        finished_at, priority,
-        accepts_staff, created_at, edited_at)
+        finished_at, priority, accepts_staff,
+        created_at, edited_at)
         VALUES
         (task0_id, event0_id, event_staff0_id, 'Prepare stage for Joe Cocker',
         NULL,
         NULL, 'medium', true,
         '2023-05-03 10:42:10.4', '2023-05-03 10:42:10.4');
+
+    INSERT INTO task
+        (id, event_id, creator_id, title,
+        description,
+        finished_at, priority, accepts_staff,
+        created_at, edited_at)
+        VALUES
+        (task1_id, event1_id, event_staff2_id, 'Unpack all guitars.',
+        'The band SharpBoots must have all guitars unpacked 6+ hours prior.',
+        NULL, 'high', true,
+        '2023-12-30 11:42', '2023-12-30 11:42');
+
 
     INSERT INTO assigned_staff
         (task_id, staff_id, decided_by, status,
@@ -243,6 +413,20 @@ BEGIN
         VALUES
         (task0_id, event_staff0_id, event_staff0_id, 'accepted',
         '2023-05-03 11:45:10.4', '2023-05-03 11:45:10.4');
+
+    INSERT INTO assigned_staff
+        (task_id, staff_id, decided_by, status,
+        created_at, edited_at)
+        VALUES
+        (task1_id, event_staff2_id, event_staff2_id, 'accepted',
+        '2023-05-03 11:45:10.4', '2023-05-03 11:45:10.4');
+
+    INSERT INTO assigned_staff
+        (task_id, staff_id, decided_by, status,
+        created_at, edited_at)
+        VALUES
+        (task1_id, event_staff4_id, NULL, 'pending',
+        '2024-01-01 11:45:20.1', '2024-01-01 11:45:20.1');
 
     INSERT INTO comment
         (id, event_id, task_id, author_id,
@@ -259,5 +443,13 @@ BEGIN
         VALUES
         (comment1_id, event0_id, NULL, user0_id,
         'Mayyyn, this event is amazing!',
+        '2023-05-03 10:00:01.5', '2023-05-03 10:05:16.1');
+
+    INSERT INTO comment
+        (id, event_id, task_id, author_id,
+        content, created_at, edited_at)
+        VALUES
+        (comment2_id, event1_id, NULL, user2_id,
+        'This gets organized for the 3rd time ;)',
         '2023-05-03 10:00:01.5', '2023-05-03 10:05:16.1');
 END $$;
