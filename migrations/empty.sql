8ddaa3ef24ac215ef876a5668e3d953d68ff9cfb
Slavomir Vlcek <svlc@inventati.org>
2024-01-02 18:51:30 +0100

    refactor(DB): finish DB emptying script
    Now, it should not trigger any errors anymore.

--- migrations/empty.sql
+++ migrations/empty.sql
@@ -1,18 +1,16 @@
 --
--- A script deleting all rows from all tables.
---
--- Note: Needs some tweaking, ATM has issues with constraints.
+-- A script for deleting all rows from all tables.
 --
 DELETE FROM assigned_staff;
-DELETE FROM event_staff;
-DELETE FROM employment;
 DELETE FROM associated_company;
-DELETE FROM company;
 DELETE FROM address;
 DELETE FROM comment;
-DELETE FROM user_record;
+DELETE FROM task; -- must be after `comment`
+DELETE FROM event_staff; -- must be after `task`
+DELETE FROM employment; -- must be after `event_staff`
 DELETE FROM work_day;
-DELETE FROM timesheet;
-DELETE FROM task;
-DELETE FROM event;
+DELETE FROM timesheet; -- must be after `work_day`, before `company`
+DELETE FROM company; -- must be after `address` and `associated_company`
+DELETE FROM user_record;
+DELETE FROM event; -- must be after `comment`
 
