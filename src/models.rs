a9f85ea5ebd84636685815b26a2334f1c9bcb623
Matej Vavrek <matvav13@gmail.com>
2024-01-05 11:31:26 +0100

    feat: fmt::display for enums

--- src/models.rs
+++ src/models.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, sqlx::Type, PartialEq, Clone)]
@@ -8,12 +10,30 @@ pub enum UserRole {
     Admin,
 }
 
+impl fmt::Display for UserRole {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            UserRole::Admin => write!(f, "Admin"),
+            UserRole::User => write!(f, "User"),
+        }
+    }
+}
+
 #[derive(Serialize, Deserialize, Debug, sqlx::Type, Clone, PartialEq)]
 #[serde(rename_all = "snake_case")]
 #[sqlx(type_name = "user_status", rename_all = "lowercase")]
 pub enum UserStatus {
     Available,
     Unavailable,
+}
+
+impl fmt::Display for UserStatus {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            UserStatus::Available => write!(f, "Available"),
+            UserStatus::Unavailable => write!(f, "Unavailable"),
+        }
+    }
 }
 
 #[derive(Serialize, Deserialize, Debug, sqlx::Type, PartialEq, Clone)]
@@ -25,12 +45,31 @@ pub enum Gender {
     Other,
 }
 
+impl fmt::Display for Gender {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Gender::Female => write!(f, "Female"),
+            Gender::Male => write!(f, "Male"),
+            Gender::Other => write!(f, "Other"),
+        }
+    }
+}
+
 #[derive(Serialize, Deserialize, Debug, sqlx::Type, PartialEq, Clone)]
 #[serde(rename_all = "snake_case")]
 #[sqlx(type_name = "event_role", rename_all = "lowercase")]
 pub enum EventRole {
     Staff,
     Organizer,
+}
+
+impl fmt::Display for EventRole {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            EventRole::Organizer => write!(f, "Event Organizer"),
+            EventRole::Staff => write!(f, "Staff"),
+        }
+    }
 }
 
 #[derive(Serialize, Deserialize, Debug, sqlx::Type, PartialEq, Clone)]
@@ -42,6 +81,16 @@ pub enum AcceptanceStatus {
     Rejected,
 }
 
+impl fmt::Display for AcceptanceStatus {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            AcceptanceStatus::Accepted => write!(f, "Accepted"),
+            AcceptanceStatus::Rejected => write!(f, "Rejected"),
+            AcceptanceStatus::Pending => write!(f, "Pending"),
+        }
+    }
+}
+
 #[derive(Serialize, Deserialize, Debug, sqlx::Type, PartialEq, Clone)]
 #[serde(rename_all = "snake_case")]
 #[sqlx(type_name = "task_priority", rename_all = "lowercase")]
@@ -49,6 +98,16 @@ pub enum TaskPriority {
     Low,
     Medium,
     High,
+}
+
+impl fmt::Display for TaskPriority {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            TaskPriority::High => write!(f, "High"),
+            TaskPriority::Medium => write!(f, "Medium"),
+            TaskPriority::Low => write!(f, "Low"),
+        }
+    }
 }
 
 #[derive(Serialize, Deserialize, Debug, sqlx::Type, Clone, PartialEq)]
@@ -61,6 +120,17 @@ pub enum Association {
     Other,
 }
 
+impl fmt::Display for Association {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Association::Sponsor => write!(f, "Sponsor"),
+            Association::Organizer => write!(f, "Organizer"),
+            Association::Media => write!(f, "Media"),
+            _ => write!(f, "Other"),
+        }
+    }
+}
+
 #[derive(Serialize, Deserialize, Debug, sqlx::Type, Clone, PartialEq)]
 #[serde(rename_all = "snake_case")]
 #[sqlx(type_name = "employee_level", rename_all = "lowercase")]
@@ -69,6 +139,16 @@ pub enum EmployeeLevel {
     Manager,
     #[sqlx(rename = "company_administrator")]
     CompanyAdministrator,
+}
+
+impl fmt::Display for EmployeeLevel {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            EmployeeLevel::Basic => write!(f, "Basic"),
+            EmployeeLevel::Manager => write!(f, "Manager"),
+            EmployeeLevel::CompanyAdministrator => write!(f, "Company Administrator"),
+        }
+    }
 }
 
 #[derive(Serialize, Deserialize, Debug, sqlx::Type, Clone, PartialEq)]
@@ -80,6 +160,16 @@ pub enum EmployeeContract {
     Hpp,
 }
 
+impl fmt::Display for EmployeeContract {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            EmployeeContract::Dpp => write!(f, "DPP"),
+            EmployeeContract::Dpc => write!(f, "DPC"),
+            EmployeeContract::Hpp => write!(f, "HPP"),
+        }
+    }
+}
+
 #[derive(Serialize, Deserialize, Debug, sqlx::Type, Clone, PartialEq)]
 #[serde(rename_all = "snake_case")]
 #[sqlx(type_name = "approval_status", rename_all = "lowercase")]
@@ -88,4 +178,15 @@ pub enum ApprovalStatus {
     Approved,
     Rejected,
     NotRequested,
+}
+
+impl fmt::Display for ApprovalStatus {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            ApprovalStatus::Approved => write!(f, "Approved"),
+            ApprovalStatus::Rejected => write!(f, "Rejected"),
+            ApprovalStatus::Pending => write!(f, "Pending"),
+            ApprovalStatus::NotRequested => write!(f, "Not Requested"),
+        }
+    }
 }
