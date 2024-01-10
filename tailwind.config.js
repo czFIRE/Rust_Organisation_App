e1fe26cf1d071de595a9d07c94e2b0aab25fd3eb
Matej Vavrek <matvav13@gmail.com>
2024-01-10 13:31:06 +0100

    feat: tailwind css config

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    colors: {
+      'background': '#171821',
+      'text': '#D2D2D2',
+      'texthl': '#00ADB5',
+      'accepted': '#00B548',
+      'rejected': '#B50B00',
+      'pending': '#B58D00',
+      'foreground1': '#222330',
+      'foreground2': '#2F3041',
+   },
+    extend: {},
+  },
+  plugins: [],
+}
+
