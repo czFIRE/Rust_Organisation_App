6edecfea85b887b4a5515b5355e0c98b1fcb9e8b
Matej Vavrek <matvav13@gmail.com>
2024-01-19 17:49:35 +0100

    fix: cargo sort :|

--- src/templates/timesheet.rs
+++ src/templates/timesheet.rs
@@ -5,7 +5,6 @@ use askama::Template;
 use chrono::{NaiveDate, NaiveDateTime};
 use serde::Deserialize;
 use sqlx::types::uuid;
-use std::collections::HashMap;
 use uuid::Uuid;
 
 use crate::utils::year_and_month::YearAndMonth;
@@ -212,66 +211,66 @@ pub struct TimesheetReviewTemplate {
     pub sheet: TimesheetTemplate,
 }
 
-#[derive(Debug, Deserialize)]
-pub struct DetailedWage {
-    // A tax value which is used for computing employee's `net wage` and such.
-    pub tax_base: f32,
-    //
-    // A final wage an employee is supposed to be given.
-    //
-    pub net_wage: f32,
+// #[derive(Debug, Deserialize)]
+// pub struct DetailedWage {
+//     // A tax value which is used for computing employee's `net wage` and such.
+//     pub tax_base: f32,
+//     //
+//     // A final wage an employee is supposed to be given.
+//     //
+//     pub net_wage: f32,
 
-    // Number of worked hours per timesheet or a whole month.
-    pub worked_hours: f32,
+//     // Number of worked hours per timesheet or a whole month.
+//     pub worked_hours: f32,
 
-    // Note: In `wage_currency` units.
-    pub employee_social_insurance: f32,
-    pub employee_health_insurance: f32,
-    pub employer_social_insurance: f32,
-    pub employer_health_insurance: f32,
-}
+//     // Note: In `wage_currency` units.
+//     pub employee_social_insurance: f32,
+//     pub employee_health_insurance: f32,
+//     pub employer_social_insurance: f32,
+//     pub employer_health_insurance: f32,
+// }
 
-impl Default for DetailedWage {
-    fn default() -> DetailedWage {
-        DetailedWage {
-            tax_base: 0.0,
-            net_wage: 0.0,
-            worked_hours: 0.0,
-            employee_social_insurance: 0.0,
-            employee_health_insurance: 0.0,
-            employer_social_insurance: 0.0,
-            employer_health_insurance: 0.0,
-        }
-    }
-}
+// impl Default for DetailedWage {
+//     fn default() -> DetailedWage {
+//         DetailedWage {
+//             tax_base: 0.0,
+//             net_wage: 0.0,
+//             worked_hours: 0.0,
+//             employee_social_insurance: 0.0,
+//             employee_health_insurance: 0.0,
+//             employer_social_insurance: 0.0,
+//             employer_health_insurance: 0.0,
+//         }
+//     }
+// }
 
-#[derive(Template, Debug, Deserialize)]
-#[template(path = "employment/timesheet/timesheet_wage_detailed.html")]
-pub struct TimesheetWageDetailed {
-    // A total wage data for selected timesheet's work.
-    pub total_wage: DetailedWage,
+// #[derive(Template, Debug, Deserialize)]
+// #[template(path = "employment/timesheet/timesheet_wage_detailed.html")]
+// pub struct TimesheetWageDetailed {
+//     // A total wage data for selected timesheet's work.
+//     pub total_wage: DetailedWage,
 
-    pub wage_currency: String,
-    pub hourly_wage: f32,
+//     pub wage_currency: String,
+//     pub hourly_wage: f32,
 
-    //
-    // A wage employee is supposed to get for selected event's work,
-    // divided into months.
-    //
-    pub month_to_detailed_wage: HashMap<YearAndMonth, DetailedWage>,
+//     //
+//     // A wage employee is supposed to get for selected event's work,
+//     // divided into months.
+//     //
+//     pub month_to_detailed_wage: HashMap<YearAndMonth, DetailedWage>,
 
-    // Note: Empty value means a wage computation went well and data are valid.
-    pub error_option: Option<String>,
-}
+//     // Note: Empty value means a wage computation went well and data are valid.
+//     pub error_option: Option<String>,
+// }
 
-impl Default for TimesheetWageDetailed {
-    fn default() -> TimesheetWageDetailed {
-        TimesheetWageDetailed {
-            total_wage: DetailedWage::default(),
-            wage_currency: "".to_string(),
-            hourly_wage: 0.0,
-            month_to_detailed_wage: HashMap::new(),
-            error_option: None,
-        }
-    }
-}
+// impl Default for TimesheetWageDetailed {
+//     fn default() -> TimesheetWageDetailed {
+//         TimesheetWageDetailed {
+//             total_wage: DetailedWage::default(),
+//             wage_currency: "".to_string(),
+//             hourly_wage: 0.0,
+//             month_to_detailed_wage: HashMap::new(),
+//             error_option: None,
+//         }
+//     }
+// }
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,4 +1,4 @@
+pub mod calculate_wage;
 pub mod format_check;
 pub mod image_storage;
-pub mod calculate_wage;
 pub mod year_and_month;
